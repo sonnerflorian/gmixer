bca6dbd944a9b289aca45e0537f0ab1a6f569b25
sonnerflorian <sonnerfl103272@th-nuernberg.de>
2025-11-05 12:54:20 +0100

    hello world tkinter

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+import tkinter as tk
+
+
+root = tk.Tk()
+root.title("Hello World")
+
+label = tk.Label(root, text="Hello, World!", font=("Arial", 24))
+label.pack(padx=40, pady=40)
+
+root.mainloop()
