cf4a91211e1d0f6b3e5e0f631d0927b9b8e84f0b
sonnerflorian <sonnerfl103272@th-nuernberg.de>
2025-11-13 16:45:14 +0100

    Initialize the project with a new init.py file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y python3 python3-pip git
-pip3 install -r requirements.txt
+
+echo "🔧 Installiere Getränkemixer..."
+
+# 1. System updaten
+sudo apt update
+sudo apt install -y python3 python3-pip python3-tk
+
+
+# 2. Desktop-Icon erstellen
+echo "[Desktop Entry]
+Type=Application
+Name=Getränkemixer
+Comment=Startet das Getränkemixer-Programm
+Exec=/usr/bin/python3 /home/gmixer/gmixer/init.py
+Path=/home/gmixer/gmixer
+Icon=/home/gmixer/gmixer/icon.jpg
+Terminal=false" > /home/gmixer/Desktop/Getraenkemixer.desktop
+
+chmod +x /home/gmixer/Desktop/Getraenkemixer.desktop
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable getraenkemixer.service
+
+echo "✅ Installation abgeschlossen!"
