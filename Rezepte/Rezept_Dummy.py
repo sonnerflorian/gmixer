88d9f6d0d840901e0fb1272c6b6e9bc4a45a775b
sonnerflorian <sonnerfl103272@th-nuernberg.de>
2025-11-23 18:30:56 +0100

    DUmmyRezept

---
+++ Rezepte/Rezept_Dummy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import time
+
+def main():
+    print("Dummy-Rezept gestartet...")
+    time.sleep(2)
+    print("Dummy-Rezept fertig.")
+
+if __name__ == "__main__":
+    main()
