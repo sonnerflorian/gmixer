f2495f29d1cde38e9c78b69ea5d104b0469ec72d
sonnerflorian <sonnerfl103272@th-nuernberg.de>
2025-12-04 15:51:09 +0100

    test

--- servotest.py
+++ servotest.py
@@ -11,18 +11,6 @@ try:
   while True:
     p.ChangeDutyCycle(5)
     time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
     p.ChangeDutyCycle(2.5)
     time.sleep(0.5)
 except KeyboardInterrupt:
