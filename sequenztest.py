c8731f3caef6631dc981c3ff1e9fa794bf43c1ee
sonnerflorian <sonnerfl103272@th-nuernberg.de>
2025-12-11 12:30:13 +0100

    d

--- sequenztest.py
+++ sequenztest.py
@@ -8,7 +8,7 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 STEP_PIN = 16 
 DIR_PIN = 20
 ENABLE_PIN = 21
-DELAY = 0.0002 # Verzögerung zwischen Pulsen (in Sekunden)
+DELAY = 0.001 # Verzögerung zwischen Pulsen (in Sekunden)
 STEPS = 1200
 
 SERVO_PINS = [26, 19, 13, 6, 5, 10, 27, 22]
