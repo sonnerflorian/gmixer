e5ab36bdee60ece2fb737e24ff5c905e471f7dcc
sonnerflorian <sonnerfl103272@th-nuernberg.de>
2026-01-08 16:01:34 +0100

    jj

--- hardware_config.py
+++ hardware_config.py
@@ -15,27 +15,3 @@ SWITCH_PIN = 2
 SWITCH_PULL = True           
 # LOW (False) = gedrückt, da der Taster mit GND verbunden ist
 SWITCH_ACTIVE_STATE = False  
-
-# Servo-Pins je Getränk (BCM)
-SERVO_PINS = {
-    "Apfelsaft": 26,
-    "Wasser": 19,
-    "Johannisbeere": 13,
-    # "Getränk4": 23,
-    # "Getränk5": 18,
-    # "Getränk6": 22,
-    # "Getränk7": 27,
-    # "Getränk8": 23,
-}
-
-DRINK_POSITIONS = {
-    "start": 0,
-    "Apfelsaft": 300,
-    "Wasser": 600,
-    "Johannisbeere": 900,
-    # "Getränk4": 1200,
-    # "Getränk5": 2000,
-    # "Getränk6": 2400,
-    # "Getränk7": 2800,
-    # "Getränk8": 3200,
-}
